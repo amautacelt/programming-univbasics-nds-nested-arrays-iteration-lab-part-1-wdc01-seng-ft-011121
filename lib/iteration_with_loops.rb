fa54f45226ce9808a5668bf4d5d28a1bdd82d506
C. L <amautacelt@gmail.com>
2020-12-28 03:39:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,9 +4,9 @@ def find_even_values(src)
   # Output all even values in each nested array
 
   row_index = 0
-  while row_index < spice_rack.count do
+  while row_index < src.count do
     element_index = 0
-    while element_index < spice_rack[row_index].count do
+    while element_index < src[row_index].count do
       p spice_rack[row_index][element_index]
       element_index += 1
     end
